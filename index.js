fdb440d8de243b9ea3200585eeeae5e4a6f1d9dd
MahmudPreyom <preyom5076@gmail.com>
2023-06-06 23:34:05 +0600

    six

--- index.js
+++ index.js
@@ -182,7 +182,7 @@ async function run() {
         // create payment intent
         app.post('/create-payment-intent', verifyJWT, async (req, res) => {
             const { price } = req.body;
-            const amount = price * 100;
+            const amount = parseInt(price * 100);
             const paymentIntent = await stripe.paymentIntents.create({
                 amount: amount,
                 currency: 'usd',
@@ -201,7 +201,72 @@ async function run() {
             const query = { _id: { $in: payment.cartItems.map(id => new ObjectId(id)) } }
             const deleteResult = await cartCollection.deleteMany(query)
 
-            res.send({insertResult, deleteResult});
+            res.send({ insertResult, deleteResult });
+        })
+
+        app.get('/admin-stats', verifyJWT, verifyAdmin, async (req, res) => {
+            const users = await usersCollection.estimatedDocumentCount();
+            const products = await menuCollection.estimatedDocumentCount();
+            const orders = await paymentCollection.estimatedDocumentCount();
+
+            // best way to get sum of the price field is to use group and sum operator
+
+            const payments = await paymentCollection.find().toArray();
+            const revenue = payments.reduce((sum, payment) => sum + payment.price, 0)
+
+            res.send({
+                revenue,
+                users,
+                products,
+                orders
+            })
+        })
+
+        /**
+         * ----------------------
+         * BANGLA SYSTEM(second best solution)
+         * ----------------------
+         * 1. load all payments
+         * 2. for each payment, get the menuItems array
+         * 3. for each item in the menuItems array get the menuItem from the menu collection
+         * 4. put them in an array: allOrderedItems
+         * 5. separate allOrderedItems by category using filter
+         * 6. now get the quantity by using length: pizza.length
+         * 7. for each category use reduce to get the total amount spent on this category
+         * 
+         */
+        app.get('/order-stats', verifyJWT, verifyAdmin, async (req, res) => {
+            const pipeline = [
+                {
+                  $lookup: {
+                    from: 'menu',
+                    localField: 'menuItems',
+                    foreignField: '_id',
+                    as: 'menuItemsData'
+                  }
+                },
+                {
+                  $unwind: '$menuItemsData'
+                },
+                {
+                  $group: {
+                    _id: '$menuItemsData.category',
+                    count: { $sum: 1 },
+                    total: { $sum: '$menuItemsData.price' }
+                  }
+                },
+                {
+                  $project: {
+                    category: '$_id',
+                    count: 1,
+                    total: { $round: ['$total', 2] },
+                    _id: 0
+                  }
+                }
+              ];
+
+            const result = await paymentCollection.aggregate(pipeline).toArray();
+            res.send(result);
         })
 
         // Send a ping to confirm a successful connection
